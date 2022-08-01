0e087b7446a47f2dc1d3b2c6050a90ea99c2c6cf
cpk0709 <cpk0709@gmail.com>
2022-08-01 11:37:51 +0900

    delete test css styles

--- components/NavBar.js
+++ components/NavBar.js
@@ -7,23 +7,10 @@ export default function NavBar() {
   return (
     <nav>
       <Link href="/">
-        <a
-          className={`${styles.link} ${
-            router.pathname === "/" ? styles.active : ""
-          }`}
-        >
-          Home
-        </a>
+        <a>Home</a>
       </Link>
       <Link href="/about">
-        <a
-          className={[
-            styles.link,
-            router.pathname === "/about" ? styles.active : "",
-          ].join(" ")}
-        >
-          About
-        </a>
+        <a>About</a>
       </Link>
     </nav>
   );
